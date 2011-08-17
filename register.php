46ff9977e7ac05a3e142240f4509d8a5a7ca63cf
Steven Lu <tacticalazn@gmail.com>
2011-08-17 01:42:10 -0400

    building register to update db with verified flag

--- register.php
+++ register.php
@@ -3,4 +3,17 @@ include('include/header.php');
 include_once('register.helpers.php');
 ?>
 
+<?php 
+if (generate_token($_GET['email']) == $_GET['code'])
+   db_query('UPDATE users WHERE user="%s" SET verified="1"', $_GET['email']);
+else
+   die();
+?>
+
+<div id="message" class="notification success" style="width: 510px; margin 0 auto;">
+   <p id="message_text">
+      <strong>You've successfully registered! Now you can go login!</strong>
+   </p>
+</div>
+
 <?php include('include/footer.php'); ?>
