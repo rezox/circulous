83d94a861be11d144453a9dbde50a6cc55aaa70c
Steven Lu <tacticalazn@gmail.com>
2011-08-23 09:57:19 -0400

    added in orchestra config

--- include/config.php
+++ include/config.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_HOST', 'localhost');
-define('DB_USER', 'circulous');
-define('DB_PASS', 'duBbVH9vjs7uwHEf');
-define('DB_NAME', 'circulous');
+define('DB_HOST', '10.194.111.8');
+define('DB_USER', 'user_0d1b43ef');
+define('DB_PASS', 'G6,&oKtoscx)zY');
+define('DB_NAME', 'db_0d1b43ef');
 
 define('FACEBOOK_APP_ID', '138740612879677');
 define('FACEBOOK_SECRET', '2ff5f3463d2e14376c92c7643308fa4e');
