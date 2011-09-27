3d83a79ec908123563741ab913c858fae893ed32
Steven Lu <tacticalazn@gmail.com>
2011-09-27 00:29:55 -0400

    added in testing-level db

--- include/config.php
+++ include/config.php
@@ -3,16 +3,22 @@ if (isset($_SERVER['db_hostname']))
 {
    define('DB_HOST', getenv('db_hostname'));
    define('DB_USER', getenv('db_username'));
-   define('DB_PASS', getenv('db_password'));
    define('DB_NAME', getenv('db_database'));
+}
+else if ($_SERVER['SERVER_ADDR'] == '173.255.228.96')
+{
+   define('DB_HOST', 'localhost');
+   define('DB_USER', 'circulous');
+   define('DB_NAME', 'circulous');
 }
 else
 {
    define('DB_HOST', '10.194.111.8');
    define('DB_USER', 'user_ab1e97b7');
-   define('DB_PASS', '8zRv5Z4sRkhLpu');
    define('DB_NAME', 'db_ab1e97b7');
 }
+
+define('DB_PASS', '8zRv5Z4sRkhLpu');
 
 define('FACEBOOK_APP_ID', '138740612879677');
 define('FACEBOOK_SECRET', '2ff5f3463d2e14376c92c7643308fa4e');
