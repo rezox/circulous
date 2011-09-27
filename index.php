f406f0c7bcaa68c19cb377fe5e1e50f6a31757ff
Steven Lu <tacticalazn@gmail.com>
2011-09-27 19:56:18 -0400

    organizing images

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php include 'include/header.php'; ?>
 <div style="margin: 20px auto 0 auto; width: 237px;">
-   <img src="resources/images/logo.png" border="0" />
+   <img src="resources/img/logo.png" border="0" />
 </div>
 
 <div style="margin: 0 auto; width: 740px; margin-top: 10px;">
    <div style="position: absolute; margin-left: 0px; width: 120px;">
-      <img src="resources/images/books.png" border="0" />
+      <img src="resources/img/books.png" border="0" />
    </div>
    <div style="position: absolute; margin-left: 140px; width: 600px" class="phrase">
       Circulo.us helps you quickly buy or sell your textbooks with people at your school so you can make the most of your money. Best of all, it's completely free.
@@ -36,27 +36,27 @@
 </div>
 
 <div style="margin: 0px auto 0 auto; width: 747px; padding-top: 0px;">
-   <img src="resources/images/bar.png" border="0" />
+   <img src="resources/img/bar.png" border="0" />
 </div>
 
 <div style="margin: -70px auto 0 auto; width: 750px;">
    <div class="infobox" style="position: absolute;">
-      <img src="resources/images/mailsr.png" valign="top" style="margin-right: 15px;" /><strong>A simple way to find the books you need.</strong><br />
+      <img src="resources/img/mailsr.png" valign="top" style="margin-right: 15px;" /><strong>A simple way to find the books you need.</strong><br />
       <div style="margin: 3px 0 0 32px;">Circulous asks for you .edu email address so it can find people at your school who are also looking to buy and sell. The result? A simple and effective way to match you with someone else on campus for a book exchange. Nice.</div>
    </div>
 
    <div class="infobox" style="position: absolute; margin-top: 135px;">
-      <img src="resources/images/check.png" valign="top" style="margin-right: 15px" /><strong>Safer.</strong><br />
+      <img src="resources/img/check.png" valign="top" style="margin-right: 15px" /><strong>Safer.</strong><br />
       <div style="margin: 3px 0 0 32px;">Because you're dealing with students in your school, you can perform an exchange on your campus; avoiding middle-man shipping and payment gateways, and perform inspections before you buy.</div>
    </div>
 
    <div class="infobox" style="position: absolute; margin-left: 400px;">
-      <img src="resources/images/moneyp.png" valign="top" style="margin-right: 15px;" /><strong>How does it all work?</strong><br />
+      <img src="resources/img/moneyp.png" valign="top" style="margin-right: 15px;" /><strong>How does it all work?</strong><br />
       <div style="margin: 3px 0 0 32px;">To sell your books, just list the condition and offer a price. We'll take care of the rest by promoting your listing to potential buyers.</div>
    </div>
 
    <div class="infobox" style="position: absolute; margin: 100px 0 0 400px;">
-      <img src="resources/images/moneyc.png" valign="top" style="margin-right: 15px;" /><strong>Buying is just as easy.</strong><br />
+      <img src="resources/img/moneyc.png" valign="top" style="margin-right: 15px;" /><strong>Buying is just as easy.</strong><br />
       <div style="margin: 3px 0 0 32px;">Tell us the book you want at the price you're looking for. Then we'll show you what's available. If you don't find what you want, we'll be sure to send you an email when we find your match.</div>
 	</div>
 </div>
