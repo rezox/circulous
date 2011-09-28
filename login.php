b57d2595010a1aa3c2dbbf4b73d211119634253f
Steven Lu <tacticalazn@gmail.com>
2011-09-28 12:55:27 -0400

    added in verify to login, login visual changes

--- login.php
+++ login.php
@@ -1,23 +1,31 @@
 <?php include 'include/header.php'; ?>
 
-<div style="margin: 20px auto -30px auto; width: 237px;">
+<div style="margin: 20px auto -30px auto; width: 300px; text-align: center;">
    <img src="resources/img/logo.png" />
+	<div style="font-family: Lato; font-size: 16pt;">A new way to look for books.</div>
 </div>
 
-<div id="message" class="notification warning" style="width: 460px; margin: 60px auto -25px auto;">
+<?php
+if (isset($_GET['verified']))
+	$msg = array('type' => 'success', 'text' => 'Your account has been activated, login to continue.');
+else if (isset($_GET['verify_error']))
+	$msg = array('type' => 'error', 'text' => 'Bad verification, please check the link and try again.');
+?>
+
+<div id="message" class="notification <?php if (isset($msg)) echo $msg['type']; else echo 'warning'; ?>" style="width: 460px; margin: 60px auto -25px auto;">
    <p id='message_text'>
-      <b>Please login to continue.</b> 
+      <b><?php if (isset($msg)) echo $msg['text']; else echo 'Please login to continue.'; ?></b> 
    </p>
 </div>
 
 <div id="formholder">
-	<form>
+	<form action="login.php?process" method="POST">
 		<ul class="formul">
 			<li>
 				<span class="intyp">
 					Email
 				</span>
-				<input type="text" name="email" />
+				<input type="text" name="email" value="<?php if (isset($_POST)) echo $_POST['email']; ?>"/>
 			</li>
 			<li>
 				<span class="intyp">
--- register.verify.php
+++ register.verify.php
@@ -5,29 +5,23 @@ include_once('register.helpers.php');
 
 <?php 
 if (!isset($_GET['email']) || !isset($_GET['code']))
-   $message = array('type' => 'error', 'message' => 'Not a properly encoded verification link.');   
+   header('Location: login.php?verify_error');
 else
 {
    $response = db_query('SELECT COUNT(*) FROM users WHERE email="%s" AND verified="0"', $_GET['email']);
    if ($response['COUNT(*)'] !== 1)
-      $message = array('type' => 'error', 'message' => 'No such user registration needed verification.'); 
+      header('Location: login.php?verified');
    else
    {
       if (generate_token($_GET['email']) == $_GET['code'])
       {
          db_query('UPDATE users WHERE email="%s" SET verified="1"', $_GET['email']);
-         $message = array('type' => 'success', 'message' => 'Successfully verified, you may now login.');
+         header('Location: login.php?verified');
       }
       else
-         $message = array('type' => 'error', 'message' => 'Email and code mismatch.');        
+         header('Location: login.php?verify_error');      
    }
 }
 ?>
-
-<div id="message" class="notification <?= $message['type']; ?>" style="width: 510px; margin 0 auto;">
-   <p id="message_text">
-      <strong><?= $message['message'] ?></strong>
-   </p>
-</div>
 
 <?php include('include/footer.php'); ?>
