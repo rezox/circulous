41378f8ad5a86c935ed1e8f39efd2fe5f9fe8dc5
Steven Lu <tacticalazn@gmail.com>
2011-09-28 02:42:56 -0400

    styled a login box

--- login.php
+++ login.php
@@ -1,7 +1,35 @@
 <?php include 'include/header.php'; ?>
 
+<div style="margin: 20px auto -30px auto; width: 237px;">
+   <img src="resources/img/logo.png" />
+</div>
 
+<div id="message" class="notification warning" style="width: 460px; margin: 60px auto -25px auto;">
+   <p id='message_text'>
+      <b>Please login to continue.</b> 
+   </p>
+</div>
 
-
+<div id="formholder">
+	<form>
+		<ul class="formul">
+			<li>
+				<span class="intyp">
+					Email
+				</span>
+				<input type="text" name="email" />
+			</li>
+			<li>
+				<span class="intyp">
+					Password
+				</span>
+				<input type="password" name="password" />
+			</li>
+			<li>
+				<input type="submit" name="submit" value="Login" />
+			</li>
+		</ul>
+	</form>
+</div>
 
 <?php include 'include/fooder.php'; ?>
